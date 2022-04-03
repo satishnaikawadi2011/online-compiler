6b3977b5b0e152c7c5608873c24c8a3f623fa519
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-03 05:57:07 +0530

    ✨ feat : add a run source function for C language

---
+++ src/languages/c/run-source.ts
@@ -0,0 +1,25 @@
+import { Result } from './../../types/result';
+import { compileCSource } from './compile-source';
+import { Options } from '../../types';
+import { runExecutable } from '../../utils/executable/execute-executable';
+import { removeFiles } from '../../utils/remove-files';
+
+/**
+ * 
+ * @param sourceCode  Source Code of a C Language to be run
+ * @param options Any extra options or spawn like arguments
+ * @returns Result object containing all the info like stdout,stderr,etc.
+ */
+export async function runCSource(sourceCode: string, options?: Options): Promise<Result> {
+	try {
+		let [
+			filePath,
+			executablePath
+		] = await compileCSource(sourceCode, options);
+		const res = await runExecutable(executablePath, options);
+		await removeFiles(executablePath, filePath);
+		return res;
+	} catch (err) {
+		return err;
+	}
+}
