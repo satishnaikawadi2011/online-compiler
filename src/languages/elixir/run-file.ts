650c80a43d10424073b759f9f167dfc9821ae211
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-09 06:51:27 +0530

    ✨ feat : add a run file function for elixir language

---
+++ src/languages/elixir/run-file.ts
@@ -0,0 +1,26 @@
+import * as path from 'path';
+import { execute } from '../../execute-command';
+import { Options, Result } from '../../types';
+
+/**
+ * 
+ * @param filePath Absolute path of the file to be run
+ * @param options Any extra options or arguments like in case of spawn
+ * @returns Result object containing all the info like stdout,stderr,etc.
+ */
+export async function runElixirFileAndReturnPromise(filePath: string, options?: Options): Promise<Result> {
+	//Make the path absolute
+	filePath = path.resolve(filePath);
+	const executionPath = 'elixir';
+	let res = await execute(
+		executionPath,
+		[
+			filePath
+		],
+		options
+	);
+	if (res.exitCode != 0) {
+		res.errorType = 'run-time';
+	}
+	return res;
+}
