05b78bb6189d3d8e4112ee0979bd3bf4beef422e
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-16 22:04:30 +0530

    💄 feat : add dark theme object

---
+++ client/src/styles/themes/dark.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from 'styled-components';
+export const darkTheme: DefaultTheme = {
+	palette:
+		{
+			common:
+				{
+					black: '#000000',
+					white: '#ffffff',
+					backdropColor: 'rgba(25, 25, 26,0.75)',
+					bodyBgColor: '#19202b'
+				},
+			// primary: '#7357fa',
+			primary: 'rgb(115, 87, 250)',
+			primaryHoverBg: '#5336e2'
+		}
+};
