c63b00f1de0843c77f94554c35014bd2a05b3a76
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-16 22:03:12 +0530

    🏷️ chore : add types for Custom Theme of styled-components

---
+++ client/src/styles/types/index.ts
@@ -0,0 +1,18 @@
+export interface DefaultTheme {
+	palette: {
+		common: {
+			black: string;
+			white: string;
+			/**
+			 * Use this for the background color of the body
+			 */
+			bodyBgColor: string;
+			/**
+			 * Use this for the backdrop color 
+			 */
+			backdropColor: string;
+		};
+		primary: string;
+		primaryHoverBg: string;
+	};
+}
