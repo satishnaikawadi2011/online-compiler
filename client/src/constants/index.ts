3fac301f8e7a49e3e36abb64cbb6e60afeafbc0f
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-17 15:11:23 +0530

    ✨ feat : add some constants to client

---
+++ client/src/constants/index.ts
@@ -0,0 +1,5 @@
+import { languages } from './languages';
+
+export const BACKEND_DEV_URL = `http://localhost:4000/api`;
+
+export { languages };
---
+++ client/src/constants/languages.ts
@@ -0,0 +1,13 @@
+export type LanguageNameType = 'cpp' | 'java' | 'c' | 'python' | 'go' | 'node' | 'rust' | 'ruby' | 'elixir';
+
+export const languages: Record<string, LanguageNameType> = {
+	'C++': 'cpp',
+	Java: 'java',
+	C: 'c',
+	Python: 'python',
+	Go: 'go',
+	'Node JS': 'node',
+	Rust: 'rust',
+	Ruby: 'ruby',
+	Elixir: 'elixir'
+};
